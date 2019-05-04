52be79344dc3da838fdfd6a6c1a0232b88a9a238
nickbigeye <yijiand2@illinois.edu>
2019-05-04 11:46:34 -0500

    Slides, nvprof test, marking affected improved by narrowing range of search

--- C/template.cu
+++ C/template.cu
@@ -43,7 +43,6 @@ __global__ void markAll(int *affected, int currEdges, int flag) {
 
 __global__ void shortUpdate(int *edgeSrc, int *edgeDst, int *to_delete_data, int currEdge){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-//    printf("Hello from short update!\n");
     if (i < currEdge){
         if (to_delete_data[i] == 1){
             to_delete_data[i] = 0;
@@ -63,43 +62,37 @@ __global__ void checkAffectedEdges(
         int * rowPtr,
         int k
         ){
-//    printf("From CheckAffectedEdges!\n");
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (i < affected_data_length){
         if (e_aff_data[i] == 1 && edgeSrc[i] != -1 && edgeDst[i] != -1) {
-            int src = edgeSrc[i];
-            int dst = edgeDst[i];
+            int src = edgeSrc[i]; // u
+            int dst = edgeDst[i]; // v
             int tcSet[200];
             int tc = triangle_counts_set(edgeSrc, edgeDst, rowPtr, tcSet, src, dst);
+//            printf("Triangle counting for %dth edge is: %d\n.", i, tc);
 
             if (tc < k - 2) {
-//                printf("%dth edge marked as delete, with tc %d\n", i, tc);
                 to_delete_data[i] = 1;
-                for (int j = 0; j < affected_data_length; ++j) {
-                    if (edgeSrc[j] == src) {
-                        for (int l = 0; l < tc; ++l) {
-                            if (edgeDst[j] == tcSet[l]) {
-                                affected_data[j] = 1;
-                            }
+
+                for (int l = 0; l < tc; ++l){
+                    int w = tcSet[l];
+                    // check if (u, w) is in edge
+                    for (int j = rowPtr[src]; j < rowPtr[src+1]; ++j){
+                        if (edgeDst[j] == w){
+                            affected_data[j] = 1;
                         }
-                    } else if (edgeDst[j] == src) {
-                        for (int l = 0; l < tc; ++l) {
-                            if (edgeSrc[j] == tcSet[l]) {
-                                affected_data[j] = 1;
-                            }
+                    }
+                    // check if (v, w) is in edge
+                    for (int j = rowPtr[dst]; j < rowPtr[dst+1]; ++j){
+                        if (edgeDst[j] == w){
+                            affected_data[j] = 1;
                         }
-                    } else if (edgeSrc[j] == dst) {
-                        for (int l = 0; l < tc; ++l) {
-                            if (edgeDst[j] == tcSet[l]) {
-                                affected_data[j] = 1;
-                            }
-                        }
-                    } else if (edgeDst[j] == dst) {
-                        for (int l = 0; l < tc; ++l) {
-                            if (edgeSrc[j] == tcSet[l]) {
-                                affected_data[j] = 1;
-                            }
+                    }
+                    // check if (w, u) or (w, v) is in edge
+                    for (int j = rowPtr[w]; j < rowPtr[w+1]; ++j) {
+                        if (edgeDst[j] == src || edgeDst[j] == dst){
+                            affected_data[j] = 1;
                         }
                     }
                 }
@@ -113,7 +106,6 @@ __global__ void selectAff(int * e_aff_data, int * affected_data, int currEdges, 
     if (i < currEdges){
         e_aff_data[i] = 0; // first set the initial value of e_aff to be zero
         if (affected_data[i] == 1) {
-//            printf("%dth edge marked as affected!\n", i);
             e_aff_data[i] = 1; // then update it to be 1 if it's affected
             atomicAdd(length, 1);
         }
@@ -153,14 +145,19 @@ int k_truss(
 
     int k = 3;
 
-    int * length = (int *) malloc (sizeof(int));
-    int * edgeCount = (int *) malloc (sizeof(int));
+//    int * length = (int *) malloc (sizeof(int));
+//    int * edgeCount = (int *) malloc (sizeof(int));
+
+    pangolin::Vector<int> length;
+    length.push_back(0);
+    pangolin::Vector<int> edgeCount;
+    edgeCount.push_back(0);
 
     int * edgeSrcDevice;
     int * edgeDstDevice;
     int * rowPtrDevice;
-    int * lengthDevice;
-    int * edgeCountDevice;
+//    int * lengthDevice;
+//    int * edgeCountDevice;
     int * affectedDevice;
     int * to_deleteDevice;
     int * e_affDevice;
@@ -171,8 +168,8 @@ int k_truss(
     cudaMalloc((void**) &to_deleteDevice, numEdges * sizeof(int));
     cudaMalloc((void**) &e_affDevice, numEdges * sizeof(int));
     cudaMalloc((void**) &rowPtrDevice, numRow * sizeof(int));
-    cudaMalloc((void**) &lengthDevice, 1 * sizeof(int)); // keep track of the length of e_aff;
-    cudaMalloc((void**) &edgeCountDevice, 1 * sizeof(int)); // keep track of the length of E after long update;
+//    cudaMalloc((void**) &lengthDevice, 1 * sizeof(int)); // keep track of the length of e_aff;
+//    cudaMalloc((void**) &edgeCountDevice, 1 * sizeof(int)); // keep track of the length of E after long update;
 
     cudaMemcpy(edgeSrcDevice, edgeSrc, numEdges * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(edgeDstDevice, edgeDst, numEdges * sizeof(int), cudaMemcpyHostToDevice);
@@ -190,14 +187,12 @@ int k_truss(
         while (true) {
 
             // select e_aff out of affected
-            length[0] = 0;
-            cudaMemcpy(lengthDevice, length, sizeof(int), cudaMemcpyHostToDevice);
-            selectAff<<<dimGrid, dimBlock>>>(e_affDevice, affectedDevice, numEdges, lengthDevice);
+            length.data()[0] = 0;
+            selectAff<<<dimGrid, dimBlock>>>(e_affDevice, affectedDevice, numEdges, length.data());
             cudaDeviceSynchronize();
-            cudaMemcpy(length, lengthDevice, sizeof(int), cudaMemcpyDeviceToHost);
 //            printf("select aff!\n");
 
-            if (length[0] == 0) {
+            if (length.data()[0] == 0) {
                 break;
             }
 
@@ -223,35 +218,15 @@ int k_truss(
         }
 
         // long update
-        edgeCount[0] = 0;
-        cudaMemcpy(edgeCountDevice, edgeCount, 1 * sizeof(int), cudaMemcpyHostToDevice);
-        countEdges<<<dimGrid, dimBlock>>>(edgeSrcDevice, numEdges, edgeCountDevice);
+        edgeCount.data()[0] = 0;
+        countEdges<<<dimGrid, dimBlock>>>(edgeSrcDevice, numEdges, edgeCount.data());
         cudaDeviceSynchronize();
-//        printf("\n");
-        cudaMemcpy(edgeCount, edgeCountDevice, 1 * sizeof(int), cudaMemcpyDeviceToHost);
-        cudaMemcpy(edgeSrc, edgeSrcDevice, numEdges * sizeof(int), cudaMemcpyDeviceToHost);
-        cudaMemcpy(edgeDst, edgeDstDevice, numEdges * sizeof(int), cudaMemcpyDeviceToHost);
 
 //        printf("Long update done!\n");
 
-//        printf("Src nodes:\n");
-//        for (int i = 0; i < numEdges; ++i){
-//            printf("%d, ", edgeSrc[i]);
-//        }
-//
-//        printf("\n");
-//        printf("Dst nodes:\n");
-//        for (int i = 0; i < numEdges; ++i){
-//            printf("%d, ", edgeDst[i]);
-//        }
-//        printf("\n");
-
-        if (edgeCount[0] > 0) {
-            printf("Order of truss is %d. Remaining # of edges is %d\n", k, edgeCount[0]);
+        if (edgeCount.data()[0] > 0) {
+            printf("Order of truss is %d. Remaining # of edges is %d\n", k, edgeCount.data()[0]);
             k += 1;
-            if (k > 10){
-                break;
-            }
         } else {
             k -= 1;
             break;
@@ -262,8 +237,8 @@ int k_truss(
     cudaFree(edgeSrcDevice);
     cudaFree(edgeDstDevice);
     cudaFree(rowPtrDevice);
-    cudaFree(lengthDevice);
-    cudaFree(edgeCountDevice);
+//    cudaFree(lengthDevice);
+//    cudaFree(edgeCountDevice);
 
     return k;
 }
@@ -271,7 +246,7 @@ int k_truss(
 int do_k_truss(pangolin::COOView<int> view) {
 
 //    // test case for our own
-    printf("For the first graph:\n");
+    printf("For the first mini-test graph:\n");
     int numEdges = 16;
     int edgeSrc[16] = {0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4};
     int edgeDst[16] = {1, 2, 0, 2, 3, 4, 0, 1, 3, 4, 1, 2, 4, 1, 2, 3};
@@ -279,7 +254,7 @@ int do_k_truss(pangolin::COOView<int> view) {
     int numRow = 6;
     printf("k-truss order for the first graph is: %d\n", k_truss(edgeSrc, edgeDst, rowPtr, numEdges, numRow));
 
-    printf("For the second graph:\n");
+    printf("For the second mini-test graph:\n");
     int numEdges2 = 30;
     int edgeSrc2[30] = {0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8};
     int edgeDst2[30] = {1, 2, 7, 0, 3, 4, 0, 5, 6, 7, 1, 4, 8, 1, 3, 5, 8, 2, 4, 6, 2, 5, 7, 8, 0, 2, 6, 3, 4, 6};
@@ -287,13 +262,13 @@ int do_k_truss(pangolin::COOView<int> view) {
     int numRow2 = 10;
     printf("k-truss order for the second graph is %d\n", k_truss(edgeSrc2, edgeDst2, rowPtr2, numEdges2, numRow2));
 
-    printf("For the third graph - California Road Network:\n");
+    printf("For the real input graph - California Road Network:\n");
     int numEdges3 = view.nnz();
     int * edgeSrc3 = const_cast<int*>(view.row_ind());
     int * edgeDst3 = const_cast<int*>(view.col_ind());
     int * rowPtr3 = const_cast<int*>(view.row_ptr());
     int numRow3 = view.num_rows();
-    printf("number of edges: %d\nnumber of rows: %d\n", numEdges3, numRow3);
+    printf("number of edges: %d\nnumber of nodes: %d\n", numEdges3, numRow3);
     printf("k-truss order for the input graph is %d\n", k_truss(edgeSrc3, edgeDst3, rowPtr3, numEdges3, numRow3));
     return 0;
 }
