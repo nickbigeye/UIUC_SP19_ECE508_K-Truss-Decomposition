385a4006e5fbd2743d04cc3caed27915d3ffacd9
nickbigeye <yijiand2@illinois.edu>
2019-05-10 01:45:07 -0500

    2 kernels GPU nvprof test

--- C/template.cu
+++ C/template.cu
@@ -6,16 +6,17 @@
 
 // neighbor-set-intersection-based triangle counting kernel
 
-pangolin::Vector<int> triangle_counts(
-        int *edgeDst,
-        int *rowPtr,
-        int u, int v) {
+__device__ int triangle_counts_set(
+        const int *edgeDst,
+        const int *rowPtr,
+        int *W,
+        const int u, const int v) {
     int u_start = rowPtr[u], u_end = rowPtr[u + 1];
     int v_start = rowPtr[v], v_end = rowPtr[v + 1];
     int w1 = edgeDst[u_start];
     int w2 = edgeDst[v_start];
-    pangolin::Vector<int> W;
-    while (u_start < u_end and v_start < v_end) {
+    int tc = 0;
+    while (u_start < u_end && v_start < v_end) {
         if (w1 == -1 || w1 < w2) {
             w1 = edgeDst[++u_start];
         }
@@ -23,100 +24,152 @@ pangolin::Vector<int> triangle_counts(
             w2 = edgeDst[++v_start];
         }
         if (w1 != -1 && w2 != -1 && w1 == w2) {
-            W.push_back(w1);
+            W[tc] = w1;
+            tc += 1;
             w1 = edgeDst[++u_start];
             w2 = edgeDst[++v_start];
         }
     }
-    return W;
+    return tc;
+}
+
+__global__ void markAll(int *affected, int currEdges, int flag) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index < currEdges) {
+        affected[index] = flag;
+    }
+}
+
+__global__ void checkAffectedEdges(
+        int * affected_data,
+        int affected_data_length,
+        int * edgeSrc,
+        int * edgeDst,
+        int * to_delete_data,
+        int * e_aff_data,
+        int * rowPtr,
+        int k
+){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < affected_data_length){
+        if (e_aff_data[i] == 1 && edgeSrc[i] != -1 && edgeDst[i] != -1) {
+            int src = edgeSrc[i];
+            int dst = edgeDst[i];
+            int tcSet[200];
+            int tc = triangle_counts_set(edgeDst, rowPtr, tcSet, src, dst);
+            if (tc < k - 2) {
+                to_delete_data[i] = 1;
+
+                for (int l = 0; l < tc; ++l) {
+                    int w = tcSet[l];
+                    // check if (u, w) is in edge
+                    for (int j = rowPtr[src]; j < rowPtr[src + 1]; ++j) {
+                        if (edgeDst[j] == w) {
+                            affected_data[j] = 1;
+                        }
+                    }
+                    // check if (v, w) is in edge
+                    for (int j = rowPtr[dst]; j < rowPtr[dst + 1]; ++j) {
+                        if (edgeDst[j] == w) {
+                            affected_data[j] = 1;
+                        }
+                    }
+                    // check if (w, u) or (w, v) is in edge
+                    for (int j = rowPtr[w]; j < rowPtr[w + 1]; ++j) {
+                        if (edgeDst[j] == src || edgeDst[j] == dst) {
+                            affected_data[j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 int k_truss(
         int *edgeSrc,         //!< node ids for edge srcs
         int *edgeDst,         //!< node ids for edge dsts
         int *rowPtr,          //!< source node offsets in edgeDst
-        int numEdges                  //!< how many edges to count triangles for
+        int numEdges,
+        int numRow
 ) {
 
     int k = 3;
 
-    // initialize the affected vector
-    pangolin::Vector<int> affected;
-    for (int i = 0; i < numEdges; ++i){
+    int * edgeSrcDevice;
+    int * edgeDstDevice;
+    int * rowPtrDevice;
+
+    cudaMalloc((void**) &edgeSrcDevice, numEdges * sizeof(int));
+    cudaMalloc((void**) &edgeDstDevice, numEdges * sizeof(int));
+    cudaMalloc((void**) &rowPtrDevice, numRow * sizeof(int));
+
+    pangolin::Vector<int> affected; // a numEdges long vector containing whether the ith edge is affected
+    pangolin::Vector<int> to_delete;
+    pangolin::Vector<int> e_aff;
+
+    // initialize all data
+    for (int i = 0; i < numEdges; i++) {
         affected.push_back(0);
+        to_delete.push_back(0);
+        e_aff.push_back(0);
     }
 
     while (true) {
 
-        // mark all e in edges as "affected"
+        // mark all edge to be affected and un-deleted if valid edges
+
         for (int i = 0; i < numEdges; i++) {
             if (edgeSrc[i] != -1){
                 affected.data()[i] = 1;
+                to_delete.data()[i] = 0;
+                e_aff.data()[i] = 0;
             }
         }
 
         while (true) {
-            pangolin::Vector<int> e_aff; // contains the index of affected edges
-            pangolin::Vector<int> to_delete; // contains the index of to-be-deleted edges
+
+            bool flag = true;
+
+            cudaMemcpy(edgeSrcDevice, edgeSrc, numEdges * sizeof(int), cudaMemcpyHostToDevice);
+            cudaMemcpy(edgeDstDevice, edgeDst, numEdges * sizeof(int), cudaMemcpyHostToDevice);
+            cudaMemcpy(rowPtrDevice, rowPtr, numRow * sizeof(int), cudaMemcpyHostToDevice);
 
             for (int i = 0; i < numEdges; ++i) {
+                e_aff.data()[i] = 0; // first set the initial value of e_aff to be zero
                 if (affected.data()[i] == 1) {
-                    e_aff.push_back(i);
+                    e_aff.data()[i] = 1; // then update it to be 1 if it's affected
+                    flag = false;        // and set break flag to be false
                 }
             }
 
-            if (e_aff.size() == 0) {
+            if (flag) {
                 break;
             }
 
-            //mark all e as "not affected"
-            for (int i = 0; i < numEdges; ++i) {
-                affected.data()[i] = -1;
-            }
+            // mark all e as "not affected"
+            dim3 dimBlock(512, 1, 1);
+            dim3 dimGrid(ceil(affected.size()/512.0), 1, 1);
+            markAll<<<dimGrid, dimBlock>>>(affected.data(), affected.size(), -1);
+            cudaDeviceSynchronize();
 
-            for (int i = 0; i < e_aff.size(); ++i) {
-                int src = edgeSrc[e_aff.data()[i]];
-                int dst = edgeDst[e_aff.data()[i]];
-                if (src != -1 && dst != -1){
-                    pangolin::Vector<int> tcSet = triangle_counts(edgeDst, rowPtr, src, dst);
-                    int tc = tcSet.size();
-                    if (tc < k - 2) {
-                        // stage 1
-                        to_delete.push_back(e_aff.data()[i]);
+            // check all affected edges
+            checkAffectedEdges<<<dimGrid, dimBlock>>>(
+                    affected.data(), affected.size(), edgeSrcDevice, edgeDstDevice,
+                            to_delete.data(), e_aff.data(),
+                            rowPtrDevice, k);
+            cudaDeviceSynchronize();
 
-                        // stage 2
-                        for (int l = 0; l < tc; ++l){
-                            int w = tcSet.data()[l];
-                            // check if (u, w) is in edge
-                            for (int j = rowPtr[src]; j < rowPtr[src+1]; ++j){
-                                if (edgeDst[j] == w){
-                                    affected.data()[j] = 1;
-                                }
-                            }
-                            // check if (v, w) is in edge
-                            for (int j = rowPtr[dst]; j < rowPtr[dst+1]; ++j){
-                                if (edgeDst[j] == w){
-                                    affected.data()[j] = 1;
-                                }
-                            }
-                            // check if (w, u) or (w, v) is in edge
-                            for (int j = rowPtr[w]; j < rowPtr[w+1]; ++j) {
-                                if (edgeDst[j] == src || edgeDst[j] == dst){
-                                    affected.data()[j] = 1;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
             // short update
-            for (int i = 0; i < to_delete.size(); ++i) {
-                edgeSrc[to_delete.data()[i]] = -1;
-                edgeDst[to_delete.data()[i]] = -1;
+            for (int i = 0; i < numEdges; ++i) {
+                if (to_delete.data()[i] == 1){
+                    edgeSrc[i] = -1;
+                    edgeDst[i] = -1;
+                }
             }
         }
 
-        // no long update any more
         int currEdges = 0;
         for (int i = 0; i < numEdges; ++i){
             if (edgeSrc[i] != -1){
@@ -125,51 +178,47 @@ int k_truss(
         }
 
         if (currEdges > 0) {
-            printf("Order of truss is %d. Remaining # of edges is %d\n", k, currEdges);
+            printf("Order of truss is %d. # of edges remaining is %d.\n", k, currEdges);
             k += 1;
+            if (k > 5){
+                break;
+            }
         } else {
             break;
         }
     }
+    cudaFree(edgeSrcDevice);
+    cudaFree(edgeDstDevice);
+    cudaFree(rowPtrDevice);
 
     return k - 1;
 }
 
 int do_k_truss(pangolin::COOView<int> view) {
-// REQUIRED
 
-//@@ create a pangolin::Vector (uint64_t) to hold per-edge triangle counts
-// Pangolin is backed by CUDA so you do not need to explicitly copy data between host and device.
-// You may find pangolin::Vector::data() function useful to get a pointer for your kernel to use.
-//        uint64_t numEdges = view.nnz();
-//        const uint32_t * edgeSrc = view.row_ind();
-//        const uint32_t * edgeDst = view.col_ind();
-//        const uint32_t * rowPtr  = view.row_ptr();
-
-// test case for our own
+    // test case for our own
 //    printf("For the first graph:\n");
 //    int numEdges = 16;
 //    int edgeSrc[16] = {0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4};
 //    int edgeDst[16] = {1, 2, 0, 2, 3, 4, 0, 1, 3, 4, 1, 2, 4, 1, 2, 3};
 //    int rowPtr[6] = {0, 2, 6, 10, 13, 16};
-//    printf("result for the first graph is: %d\n", k_truss(edgeSrc, edgeDst, rowPtr, numEdges));
+//    int numRow = 6;
+//    printf("k-truss order for the first graph is: %d\n", k_truss(edgeSrc, edgeDst, rowPtr, numEdges, numRow));
 //
 //    printf("For the second graph:\n");
-//    int numEdges2 = 30;
-//    int edgeSrc2[30] = {0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8};
-//    int edgeDst2[30] = {1, 2, 7, 0, 3, 4, 0, 5, 6, 7, 1, 4, 8, 1, 3, 5, 8, 2, 4, 6, 2, 5, 7, 8, 0, 2, 6, 3, 4, 6};
-//    int rowPtr2[10] = {0, 3, 6, 10, 13, 17, 20, 24, 27, 30};
-//    printf("result for the second graph is %d\n", k_truss(edgeSrc2, edgeDst2, rowPtr2, numEdges2));
+//    int numEdges2 = 24;
+//    int edgeSrc2[30] = {0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7};
+//    int edgeDst2[30] = {1, 2, 7, 0, 3, 4, 0, 5, 6, 7, 1, 4, 1, 3, 5, 2, 4, 6, 2, 5, 7, 0, 2, 6};
+//    int rowPtr2[10] = {0, 3, 6, 10, 12, 15, 18, 21, 24};
+//    int numRow2 = 9;
+//    printf("k-truss order for the second graph is %d\n", k_truss(edgeSrc2, edgeDst2, rowPtr2, numEdges2, numRow2));
 
-    printf("For the real input graph - California Road Network:\n");
     int numEdges3 = view.nnz();
     int * edgeSrc3 = const_cast<int*>(view.row_ind());
     int * edgeDst3 = const_cast<int*>(view.col_ind());
     int * rowPtr3 = const_cast<int*>(view.row_ptr());
     int numRow3 = view.num_rows();
-    printf("number of edges: %d\nnumber of nodes: %d\n", numEdges3, numRow3);
-    printf("k-truss order for the input graph is %d\n", k_truss(edgeSrc3, edgeDst3, rowPtr3, numEdges3));
 
+    printf("k-truss order for the input graph is %d\n", k_truss(edgeSrc3, edgeDst3, rowPtr3, numEdges3, numRow3));
     return 0;
-
 }
